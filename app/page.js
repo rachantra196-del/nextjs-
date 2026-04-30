0b3f46fe079c8df8c9236392624544a7c2609bb7
rachantra196-del <rachantra196@gmail.com>
2026-04-30 09:59:53 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
-const res = await fetch(
-  `${process.env.NEXT_PUBLIC_BASE_URL}/api/create-qr`,
-  {
-    method: "POST"
-  }
-);
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  "https://nextjs-ten-gamma-79.vercel.app";
+
+const res = await fetch(`${baseUrl}/api/create-qr`, {
+  method: "POST"
+});
