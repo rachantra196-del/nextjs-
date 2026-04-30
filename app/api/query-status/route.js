75c7df14aec876082b6dc712ab54b2ca097d240c
rachantra196-del <rachantra196@gmail.com>
2026-04-30 10:52:37 +0700

    Update route.js

--- app/api/query-status/route.js
+++ app/api/query-status/route.js
@@ -5,12 +5,13 @@ import crypto from "crypto";
 const GATEWAY_URL = "https://devwebpayment.kesspay.io/api/mch/v2/gateway";
 const CLIENT_SECRET = "iVK[rHVjUf-yrO-gl:WRdlv2N-)ZO!xrX!W9_=@t]6LZDx|95%dA,jI";
 
+/* ================= SIGN ================= */
 function sign(out_trade_no) {
-  const string =
+  const raw =
     "out_trade_no=" + out_trade_no +
     "&key=" + CLIENT_SECRET;
 
-  return crypto.createHash("md5").update(string).digest("hex").toUpperCase();
+  return crypto.createHash("md5").update(raw).digest("hex").toUpperCase();
 }
 
 export async function POST(req) {
