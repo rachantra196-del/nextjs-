dff3462d0ff3643b995566589ea011e9c07acf32
rachantra196-del <rachantra196@gmail.com>
2026-04-30 10:19:42 +0700

    Update route.js

--- app/api/create-qr/route.js
+++ app/api/create-qr/route.js
@@ -3,7 +3,7 @@ export const runtime = "nodejs";
 export async function POST() {
   return Response.json({
     success: true,
-    message: "API CONNECTED SUCCESSFULLY",
-    time: new Date().toISOString()
+    message: "API WORKING",
+    qr: "demo-qr"
   });
 }
