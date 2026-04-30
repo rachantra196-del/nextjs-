d28254fb1dc23f41993add5a4b56c0adb09b874c
rachantra196-del <rachantra196@gmail.com>
2026-04-30 10:18:32 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ const res = await fetch("/api/create-qr", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    total_amount: 10
-  })
+  }
 });
+
+const data = await res.json();
+console.log("API RESPONSE:", data);
