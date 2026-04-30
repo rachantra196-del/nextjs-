62925d5ba34a5338c5b049d7270af92a0ce327c5
rachantra196-del <rachantra196@gmail.com>
2026-04-30 10:19:53 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
-const res = await fetch("/api/create-qr", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
+"use client";
 
-const data = await res.json();
-console.log("API RESPONSE:", data);
+import { useState } from "react";
+
+export default function Home() {
+  const [msg, setMsg] = useState("");
+
+  const createQR = async () => {
+    try {
+      const res = await fetch("/api/create-qr", {
+        method: "POST"
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      setMsg(data.message || "done");
+    } catch (e) {
+      setMsg("network error");
+      console.log(e);
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>KessPay Demo</h1>
+      <button onClick={createQR}>Create QR Payment</button>
+      <p>{msg}</p>
+    </div>
+  );
+}
