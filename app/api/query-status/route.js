91e15054ab4bb181d6715c42c05a41654cd30eac
rachantra196-del <rachantra196@gmail.com>
2026-04-30 10:25:47 +0700

    Update route.js

--- app/api/query-status/route.js
+++ app/api/query-status/route.js
@@ -1,36 +1,38 @@
-
 export const runtime = "nodejs";
 
 import crypto from "crypto";
 
 const GATEWAY_URL = "https://devwebpayment.kesspay.io/api/mch/v2/gateway";
-
 const CLIENT_SECRET = "iVK[rHVjUf-yrO-gl:WRdlv2N-)ZO!xrX!W9_=@t]6LZDx|95%dA,jI";
 
-function createSign(data) {
-  const str =
-    `out_trade_no=${data.out_trade_no}` +
+/* ================= SIGN ================= */
+function createSign(out_trade_no) {
+  const raw =
+    `out_trade_no=${out_trade_no}` +
     `&service=webpay.acquire.queryOrder` +
     `&key=${CLIENT_SECRET}`;
 
-  return crypto.createHash("md5").update(str).digest("hex").toUpperCase();
+  return crypto.createHash("md5").update(raw).digest("hex").toUpperCase();
 }
 
+/* ================= API ================= */
 export async function POST(req) {
   try {
-    const { out_trade_no } = await req.json();
+    const { out_trade_no, token } = await req.json();
 
     const payload = {
       service: "webpay.acquire.queryOrder",
       sign_type: "MD5",
-      out_trade_no
+      out_trade_no,
+      sign: createSign(out_trade_no)
     };
-
-    payload.sign = createSign(payload);
 
     const res = await fetch(GATEWAY_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
       body: JSON.stringify(payload)
     });
 
